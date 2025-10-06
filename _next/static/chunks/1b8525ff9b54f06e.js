b7ab7939fb31d3a6b092251343877964feb49a85
sdooodly <sdooodly@users.noreply.github.com>
2025-10-06 16:21:12 +0000

    deploy: 6e3b8ccf54adb33b109ed4b5744a65bcf8147e0b

---
+++ _next/static/chunks/1b8525ff9b54f06e.js
@@ -0,0 +1,5 @@
+__turbopack_load_page_chunks__("/_error", [
+  "static/chunks/17722e3ac4e00587.js",
+  "static/chunks/a26248298a866126.js",
+  "static/chunks/turbopack-1b8d22fb0993bec4.js"
+])
---
+++ _next/static/laSQEOzHzV_DCxy8y1ACF/_buildManifest.js
@@ -0,0 +1,14 @@
+self.__BUILD_MANIFEST = {
+  "/_error": [
+    "./static/chunks/1b8525ff9b54f06e.js"
+  ],
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
